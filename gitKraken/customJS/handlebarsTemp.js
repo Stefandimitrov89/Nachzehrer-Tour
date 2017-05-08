6c3f3d2837a0d59864ecb8d7b41936924b7d025c
Unknown <mihailm956@gmail.com>
2017-05-09 00:33:40 +0300

    Adding HandleBars and Bootstrap
    Added a blank project with only bootstrap and handlebar functionality in a new folder so they can be checked/discussed.

---
+++ gitKraken/customJS/handlebarsTemp.js
@@ -0,0 +1,85 @@
+var hbTemplate = document.getElementById("hb-template-container").innerHTML;
+
+var template = Handlebars.compile(hbTemplate);
+
+var data = {
+  blocks: [
+    {
+      topDivClass: "col-md-7 col-md-push-5",
+      botDivClass: "col-md-5 col-md-pull-7",
+      featuretteHeading:"Oh yeah, it's that good. ",
+      lead: 'onec ullamcorper nulla non metus auctor fringilla. Vestibulum id lig',
+      textMuted: 'See for yourself.',
+      imgSource: 'b_1.jpg'
+    },
+    {
+      topDivClass: "col-md-7",
+      botDivClass: "col-md-5",
+      featuretteHeading:'additionalText. ',
+      lead: 'Vestibulum id lig',
+      textMuted: 'See for myself.',
+      imgSource: 'b_1.jpg'
+    }
+  ],
+  albums: [
+    {
+      className: "card	col-md-4",
+      imgSource: "album_1.jpg",
+      pText:"This is a wider card with supporting text below as a natural lead-in to additional content"
+    },
+    {
+      className: "card	col-md-4",
+      imgSource: "album_1.jpg",
+      pText:"This is a wider card with supporting text below as a natural lead-in to additional content"
+    },
+    {
+      className: "card	col-md-4",
+      imgSource: "album_1.jpg",
+      pText:"This is a wider card with supporting text below as a natural lead-in to additional content"
+    }
+  ],
+  services:[
+    {
+      className: "col-lg-3 hotels services",
+      imgSource: "a_circle.gif",
+      h2: "Hotels",
+      pText: "Choose from more than 10 hotels"
+    },
+    {
+      className: "col-lg-3 huds services",
+      imgSource: "a_circle.gif",
+      h2: "Huds",
+      pText: "Choose from more than 10 huds"
+    },
+    {
+      className: "col-lg-3 bmb services",
+      imgSource: "a_circle.gif",
+      h2: "BMB",
+      pText: "Choose from more than 10 bmb"
+    },
+    {
+      className: "col-lg-3 cruise services",
+      imgSource: "a_circle.gif",
+      h2: "Cruise",
+      pText: "Choose from more than 10 cruise"
+    }
+  ]
+};
+
+var container = document.getElementById('hb-container');
+container.innerHTML = template(data);
+
+/*
+    <div class="row">
+    {{#each services}}
+      <div class="{{className}}">
+        <img class="img-circle" src="{{imgSource}}" alt="Generic placeholder image" width="140" height="140">
+        <h2>{{h2}}</h2>
+        <p>{{pText}}</p>
+        <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+      </div>
+    {{/each}}
+    
+    </div>
+    <hr class="featurette-divider">
+*/
